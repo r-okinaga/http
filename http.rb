d242f40ae1d7666a14b92554adf05cf95de27d8a
r-okinaga <Hrnn0PDk1>
2016-07-01 06:24:26 +0900

    入力受付

--- http.rb
+++ http.rb
@@ -1,5 +1,6 @@
 require 'net/https'
 require 'openssl'
+require 'io/console'
 require 'csv'
 
 
@@ -9,10 +10,16 @@ module ZAC
     EXCLUDE_TYPE = %w(社長 派遣社員 -)
     EXCLUDE_SECTION = %w(いきいきライフ木場店 Nagomi名取店)
 
-    def login(user_name, password)
+    def login
         @http = Net::HTTP.new('signovate.jp.oro.com',443)
         @http.use_ssl = true
         @http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        print "ユーザー名を入力してください\n"
+        user_name = gets.to_s
+        print "パスワードを入力してください\n"
+        password = STDIN.noecho(&:gets)
+
         response = @http.post('/signovate/User/user_check.asp',"user_name=#{user_name}&password=#{password}&__page_id=")
 
         @cookie =  response.get_fields('set-cookie').join(';')
@@ -24,7 +31,7 @@ module ZAC
 
     #勤務表アウトプット
     def getKinmu
-        response = @http.get('https://signovate.jp.oro.com/signovate/Output/CSV/KinmuhyouCSV.asp?status=&type_meisai=1&y_date_begin=2016&m_date_begin=6&d_date_begin=1&y_date_end=2016&m_date_end=6&d_date_end=30&id_bumon=&id_member_yakushoku=&code_member=&name_member=&time_card_in_begin_h=&time_card_in_begin_m=0&time_card_in_end_h=&time_card_in_end_m=0&time_card_out_begin_h=&time_card_out_begin_m=0&time_card_out_end_h=&time_card_out_end_m=0&time_in_begin_h=&time_in_begin_m=0&time_in_end_h=&time_in_end_m=0&time_out_begin_h=&time_out_begin_m=0&time_out_end_h=&time_out_end_m=0&type_minute_other=0&minute_other_begin_h=0&minute_other_begin_m=0&minute_other_end_h=0&minute_other_end_m=0&shukei_unit_1=0&shukei_unit_2=0&shukei_unit_3=0&time_unit=1&__page_id=', 'Cookie' => @cookie)
+        response = @http.get("https://signovate.jp.oro.com/signovate/Output/CSV/KinmuhyouCSV.asp?status=&type_meisai=1&y_date_begin=2016&m_date_begin=6&d_date_begin=1&y_date_end=2016&m_date_end=6&d_date_end=30&id_bumon=&id_member_yakushoku=&code_member=&name_member=&time_card_in_begin_h=&time_card_in_begin_m=0&time_card_in_end_h=&time_card_in_end_m=0&time_card_out_begin_h=&time_card_out_begin_m=0&time_card_out_end_h=&time_card_out_end_m=0&time_in_begin_h=&time_in_begin_m=0&time_in_end_h=&time_in_end_m=0&time_out_begin_h=&time_out_begin_m=0&time_out_end_h=&time_out_end_m=0&type_minute_other=0&minute_other_begin_h=0&minute_other_begin_m=0&minute_other_end_h=0&minute_other_end_m=0&shukei_unit_1=0&shukei_unit_2=0&shukei_unit_3=0&time_unit=1&__page_id=", 'Cookie' => @cookie)
         data = CSV.parse(response.body.encode('utf-8', 'sjis'))
         filter(data)
     end
@@ -71,9 +78,8 @@ end
 
 
 begin
-    ZAC.login("r-okinaga", "")
+    ZAC.login
     ZAC.getKinmu
-
 rescue
     p "エラー"
     ZAC.logout
